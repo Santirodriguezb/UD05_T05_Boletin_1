7be894ba18383ec643cf16e839446acb3e37c65f
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-01-20 12:56:41 +0100

    Ejercicio 4

---
+++ ejercicio4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <?php
+        $x=144;
+        $y=999;
+        $z=$x * $Y;
+        echo $x." x ".$y." = ".$z."<br/>"
+        
+
+
+    ?>
+</body>
+</html>

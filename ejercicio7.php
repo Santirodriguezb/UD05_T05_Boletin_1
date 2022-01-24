3b643831e8a54120f7487682582ab236b4d1842e
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-01-24 09:41:22 +0100

    Ejercicio 7

---
+++ ejercicio7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 7</title>
+</head>
+<body>
+    <?php
+        $nombre='Santi';
+        $direccion='Pintor Colmeiro';
+        $telefono='698183973';
+        echo "$nombre <br/>";
+        echo "$direccion <br/>";
+        echo "$telefono <br/>" ;
+
+    ?>
+</body>
+</html>

b4b577bb46e8e0651851f6680792dc131c0e2c2a
Santirodriguezb <santiagorodriguezb@ieslaxeiro.com>
2022-03-14 17:02:26 +0100

    Exercicio 4

--- ejercicio5.php
+++ ejercicio5.php
@@ -4,40 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 5</title>
+    <title>exercicio 4</title>
 </head>
 <body>
     <?php
         $horario=array(
-            '8:50 - 9:40'=>array('IAW','SAD','ASX','ASX','IAW'),
-            '9:40 - 10:30'=>array('IAW','SAD','EEA','ASX','SRI',),
-            '10:30 - 11:20'=>array('SRI','SAD','EEA','EEA','SRI',),
-            '11:20 - 12:00'=>array('RECREO'),
-            '12:00 - 12:50'=>array('SRI','ASO','ASO','IAW','SAD'),
-            '12:50 - 13:40'=>array('ASO','ASO','IAW','IAW','SAD'),
-            '13:40 - 14:30'=>array('ASX','ASO','IAW','SRI','SAD'),
-            '16:00 - 16:50'=>array('','SRI','','ASO',''),
-            '16:00 - 16:50'=>array('','SRI','','ASO',''),
-            '16:00 - 16:50'=>array('','SRI','','ASO',''),
+            '8:50-9:40' => array('IAW','SAD','ASX','ASX','IAW'),
+            '9:40-10:30' => array('IAW','SAD','EIEA','ASX','SRI'),
+            '10:30-11:20' => array('SRI','SAD','EIEA','EIEA','SRI'),
+            '11:20-12:00' => array('Recreo'),
+            '12:00-12:50' => array('SRI','ASO','ASO','IAW','SAD'),
+            '12:50-13:40' => array('ASO','ASO','IAW','IAW','SAD'),
+            '13:40-14:30' => array('ASX','ASO','IAW','SRI','SAD'),
+            '14:30-16:00' => array('Mediodía'),
+            '16:00-16:50' => array('','SRI','','ASO',''),
+            '16:50-17:40' => array('','SRI','','ASO',''),
+            '17:50-18:30' => array('','SRI','','ASO',''),
         );
 
-        echo "<table border='1'>";
-        echo "<tr></tr><th>LUNS</th><th>MARTES</th><th>MERCORES</th><th>XOVES</th><th>VENRES</th>";
-        foreach ( $horario as $hora => $modulos ) {
-            echo "<tr>";
-                echo "<th>.$hora.</th>";
-            echo "</tr>";
-            foreach ($modulos as $m){
-                if(count($modulos)==1){
-
-                    echo "<td colspan = '5'>".$m."</td>";
-
+            echo "<table border='1'>";
+            echo "<tr><th></th><th>Luns</th><th>Martes</th><th>Mercores</th><th>Xoves</th><th>Venres</th></tr>";
+            foreach($horario as $hora => $modulos){
+                echo "<tr>";
+                echo "<th>".$hora."</th>";
+                foreach($modulos as $m){
+                    if(count($modulos)==1){
+                        echo "<td colspan='5'>".$m."</td>";
+                    }else{
+                        echo "<td>".$m."</td>";
+                    }
                 }
-            }
 
-        }
-        echo "</table>";
-        
+                echo "</tr>";
+            }
+            echo "</table>";
+
     ?>
+
 </body>
 </html>
